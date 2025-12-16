365bda9dad18b37c7b3458c1576b044f1582a2e4
Juan Natali <juan_natali@hotmail.com>
2025-12-16 15:01:20 +0000

    Updated font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ["Space Grotesk", "Inter", "system-ui", "sans-serif"],
+			},
 			borderRadius: {
 				lg: "var(--radius)",
 				md: "calc(var(--radius) - 2px)",
